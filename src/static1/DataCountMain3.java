3da6dcf7d1ac5c829f57c136eabd577529c028b9
hmkim199 <hmkim199@gmail.com>
2024-11-19 00:47:19 +0900

    섹션 8

---
+++ src/static1/DataCountMain3.java
@@ -0,0 +1,22 @@
+package static1;
+
+public class DataCountMain3 {
+    public static void main(String[] args) {
+        Data3 data1 = new Data3("A");
+        System.out.println("Data3.count = " + Data3.count);
+
+        Data3 data2 = new Data3("B");
+        System.out.println("Data3.count = " + Data3.count);
+
+        Data3 data3 = new Data3("C");
+        System.out.println("Data3.count = " + Data3.count);
+
+        //추가
+        //인스턴스를 통한 접근
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count); //가능하나 권장하지 않음.
+
+        //클래스를 통한 접근
+        System.out.println(Data3.count);
+    }
+}
